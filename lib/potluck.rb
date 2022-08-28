fc7b021aae96039ac7a1299eb6da8cd793475f33
Eli Fuchsman <efuchsman@gmail.com>
2022-08-28 16:30:58 -0600

    Create get all from category method

--- lib/potluck.rb
+++ lib/potluck.rb
@@ -14,5 +14,20 @@ class Potluck
     @dishes
   end
 
+  def get_all_from_category(category)
+    if category == :appetizer
+      @dishes.select do |dish|
+      dish.category == :appetizer
+      end
+    elsif category == :entre
+        @dishes.select do |dish|
+          dish.category == :entre
+        end
+      elsif category == :desert
+        @dishes.select do |dish|
+          dish.category == :desert
+        end
+    end
+  end
 
 end
